7cf448066f3736f8c508df86b8c5011b2cb81ff2
Derek Walton <derekwalton19@gmail.com>
2024-03-25 00:20:56 +0000

    reverse list question

--- index.js
+++ index.js
@@ -11,3 +11,58 @@
 'use strict';
 
 // add your functions here
+
+function replaceText(elem, str){
+    elem.innerText = str;
+}
+
+function addTextTo(elem, str){
+    elem.innerText += str;
+}
+
+function moreBears(){
+    let bear = document.querySelector('#animals');
+
+    bear.setAttribute('src', 'http://placebear.com/400/200');
+    bear.setAttribute('alt', 'A bear.');
+    bear.setAttribute('title', 'A BEAR!');
+}
+
+function setId(elem, str){
+    elem.setAttribute('id', str);
+    return elem;
+}
+
+function setClass(elem, str){
+    elem.className = str;
+    return elem
+}
+
+function addAClass(elem, str){
+    elem.classList.add(str);
+    return elem;
+}
+
+function removeAClass(elem, str){
+    elem.classList.remove(str);
+    return elem;
+}
+
+function newElement(name){
+    return document.createElement(name);
+}
+
+function findElementById(id){
+    return document.querySelector('#' + id);
+}
+
+function findElementsByQuery(query){
+    return document.querySelectorAll(query);
+}
+
+function reverseList(query){
+    let list = document.querySelector(query);
+    let listItem = Array.from(list.children);
+
+    
+}
