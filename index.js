f24bace77313a30335977282640ba2f67297e195
Derek Walton <derekwalton19@gmail.com>
2024-05-10 23:01:58 +0100

    on question 15

--- index.js
+++ index.js
@@ -8,61 +8,102 @@
  * we've enabled that by default with the first line of code.
  */
 
-'use strict';
+"use strict";
 
 // add your functions here
 
-function replaceText(elem, str){
-    elem.innerText = str;
+function replaceText(elem, str) {
+  elem.innerText = str;
 }
 
-function addTextTo(elem, str){
-    elem.innerText += str;
+function addTextTo(elem, str) {
+  elem.innerText += str;
 }
 
-function moreBears(){
-    let bear = document.querySelector('#animals');
+function moreBears() {
+  let bear = document.querySelector("#animals");
 
-    bear.setAttribute('src', 'http://placebear.com/400/200');
-    bear.setAttribute('alt', 'A bear.');
-    bear.setAttribute('title', 'A BEAR!');
+  bear.setAttribute("src", "http://placebear.com/400/200");
+  bear.setAttribute("alt", "A bear.");
+  bear.setAttribute("title", "A BEAR!");
 }
 
-function setId(elem, str){
-    elem.setAttribute('id', str);
-    return elem;
+function setId(elem, str) {
+  elem.setAttribute("id", str);
+  return elem;
 }
 
-function setClass(elem, str){
-    elem.className = str;
-    return elem
+function setClass(elem, str) {
+  elem.className = str;
+  return elem;
 }
 
-function addAClass(elem, str){
-    elem.classList.add(str);
-    return elem;
+function addAClass(elem, str) {
+  elem.classList.add(str);
+  return elem;
 }
 
-function removeAClass(elem, str){
-    elem.classList.remove(str);
-    return elem;
+function removeAClass(elem, str) {
+  elem.classList.remove(str);
+  return elem;
 }
 
-function newElement(name){
-    return document.createElement(name);
+function newElement(name) {
+  return document.createElement(name);
 }
 
-function findElementById(id){
-    return document.querySelector('#' + id);
+function findElementById(id) {
+  return document.querySelector("#" + id);
 }
 
-function findElementsByQuery(query){
-    return document.querySelectorAll(query);
+function findElementsByQuery(query) {
+  return document.querySelectorAll(query);
 }
 
-function reverseList(query){
-    let list = document.querySelector(query);
-    let listItem = Array.from(list.children);
+function reverseList(query) {
+  const list = document.querySelector(query);
 
-    
+  const items = list.children;
+
+//   console.log(list);
+
+  for (let i = items.length - 1; i >= 0; i--) {
+    // console.log(items[i]);
+    list.append(items[i]);
+  }
+
+//   console.log(list);
+
+  return list;
+}
+
+function mover(moveThis, appendToThis) {
+  const element = document.querySelector(moveThis);
+  const appendTo = document.querySelector(appendToThis);
+  appendTo.append(element);
+}
+
+function filler(list, candidates){
+    // const myList = document.querySelector(list);
+    // console.log(candidates);
+    candidates.forEach(candidate => {
+        const listItem = document.createElement("li");
+        listItem.textContent = candidate;
+        list.append(listItem);
+    });
+}
+
+function dupe(selector){
+    const element = document.querySelector(selector);
+
+    const newElement = document.createElement(element.nodeName);
+    newElement.textContent = element.textContent;
+    element.parentElement.appendChild(newElement);
+}
+
+function removeAll(selector){
+    const element = document.querySelectorAll(selector);
+
+
+
 }
